a5f8fd93418f84876e11c428f9b430df64d76ffc
Anurag Kumar <anuragkumar07072002@gmail.com>
2024-12-16 07:30:03 +0530

    first commit

---
+++ testgeneration/testgeneration/src/main/java/com/example/testgeneration/TestgenerationApplication.java
@@ -0,0 +1,13 @@
+package com.example.testgeneration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestgenerationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestgenerationApplication.class, args);
+	}
+
+}
---
+++ testgeneration/testgeneration/src/test/java/com/example/testgeneration/TestgenerationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testgeneration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestgenerationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
